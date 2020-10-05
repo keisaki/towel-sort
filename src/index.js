cde387d70ca1d4e9ae4c31cabdbfe2d80aff4c16
keisaki <dimakrivolapov@gmail.com>
2020-10-05 17:50:53 +0400

    fix: delete debugger

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ module.exports = function towelSort (matrix) {
   let newArr = [];
   if(matrix == undefined||matrix.length < 1){
       return newArr;
-}
+  }
   for(let  i = 0;i<matrix.length;i++)
   {
     newArr = i%2==0? newArr.concat(matrix[i]):newArr.concat(matrix[i].reverse());    
   }
-  debugger;
   return newArr;
 }
